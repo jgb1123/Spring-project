586e553afaad3575305d1dfcc054b76cd66357ed
jgb1123 <rmsqja2@gmail.com>
2022-12-13 23:09:57 +0900

    [feat] Spring Security 및 JWT 로그인 적용 중 #15 #17

--- src/main/java/com/solo/community/exception/ExceptionCode.java
+++ src/main/java/com/solo/community/exception/ExceptionCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
-    BOARD_NOT_FOUND(404, "Board not found");
+    BOARD_NOT_FOUND(404, "Board not found"),
+    NO_AUTHORIZATION_INFO(404, "No authorization information");
 
     @Getter
     private int status;
