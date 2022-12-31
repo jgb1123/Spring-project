6bb446d83792199872eaba1e79e2005b542a5e29
jgb1123 <rmsqja2@gmail.com>
2022-12-31 21:13:17 +0900

    [feat] 게시글 목록 조회 성능 개선 (페치조인) #21

--- src/main/java/com/solo/community/board/repository/BoardRepository.java
+++ src/main/java/com/solo/community/board/repository/BoardRepository.java
@@ -1,7 +1,12 @@
 package com.solo.community.board.repository;
 
 import com.solo.community.board.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    @EntityGraph(attributePaths = "member")
+    Page<Board> findAll(Pageable pageable);
 }
