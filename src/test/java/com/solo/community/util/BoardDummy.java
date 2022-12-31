488155c808a28568d9bbe62a3f0401a85d66958a
jgb1123 <rmsqja2@gmail.com>
2022-12-31 21:22:23 +0900

    [test] MemberDummy, BoardDummy 수정 #4 #12

--- src/test/java/com/solo/community/util/BoardDummy.java
+++ src/test/java/com/solo/community/util/BoardDummy.java
@@ -10,28 +10,62 @@ import java.util.ArrayList;
 
 public interface BoardDummy {
     static Board createBoard1() {
-        return new Board(1L, "글제목1", "글내용1", 0, null, new ArrayList<>());
+        return Board.builder()
+                .boardId(1L)
+                .boardTitle("글제목1")
+                .boardContent("글내용1")
+                .view(0)
+                .member(null)
+                .comments(new ArrayList<>())
+                .build();
     }
 
     static Board createBoard2() {
-        return new Board(2L, "글제목2", "글내용2", 0, null, new ArrayList<>());
+        return Board.builder()
+                .boardId(2L)
+                .boardTitle("글제목2")
+                .boardContent("글내용2")
+                .view(0)
+                .member(null)
+                .comments(new ArrayList<>())
+                .build();
     }
 
     static BoardPostDto createPostDto() {
-        return new BoardPostDto("글제목1", "글내용1");
+        return BoardPostDto.builder()
+                .boardTitle("글제목1")
+                .boardContent("글내용1")
+                .build();
     }
 
     static BoardPatchDto createPatchDto() {
-        return new BoardPatchDto("변경된 글제목", "변경된 글내용");
+        return BoardPatchDto.builder()
+                .boardTitle("변경된 글제목")
+                .boardContent("변경된 글내용")
+                .build();
     }
 
     static BoardResponseDto createdResponseDto1() {
-        return new BoardResponseDto(1L, "글제목1", "글내용1", 0, LocalDateTime.of(2022,12,18,0,0,0), LocalDateTime.of(2022,12,18,0,0,0), "hgd123");
+        return BoardResponseDto.builder()
+                .boardId(1L)
+                .boardTitle("글제목1")
+                .boardContent("글내용1")
+                .view(0)
+                .createdAt(LocalDateTime.of(2022,12,18,0,0,0))
+                .modifiedAt(LocalDateTime.of(2022,12,18,0,0,0))
+                .nickname("hgd123")
+                .build();
     }
 
     static BoardResponseDto createdResponseDto2() {
-        return new BoardResponseDto(2L, "글제목2", "글내용2", 0, LocalDateTime.of(2022,12,18,0,0,0), LocalDateTime.of(2022,12,18,0,0,0), "lss123");
+        return BoardResponseDto.builder()
+                .boardId(2L)
+                .boardTitle("글제목2")
+                .boardContent("글내용2")
+                .view(0)
+                .createdAt(LocalDateTime.of(2022,12,18,0,0,0))
+                .modifiedAt(LocalDateTime.of(2022,12,18,0,0,0))
+                .nickname("lss123")
+                .build();
     }
-
-
 }
